5f552e771540f4814a92f07dc8534b020c6aef87
abhishek bochare <abhishekbochare2003@gmail.com>
2025-10-05 17:20:30 +0530

    commit

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
-  stock_quantity: { type: Number, default: 0, min: 0 },
+  stock_quantity: { type: Number, default: 1, min: 0 },
   low_stock_threshold: { type: Number, default: 5 },
 });
 
